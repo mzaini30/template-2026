274866ca3cdba3af8b4bc72f8fa577bd724099db
mzaini30 <muhzaini30@gmail.com>
2026-05-15 14:36:21 +0800

    feat: add vConsole for debugging network fetches

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
 
 	let { children } = $props();
+
+	onMount(async () => {
+		if (browser) {
+			const VConsole = (await import('vconsole')).default;
+			new VConsole();
+		}
+	});
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
